246f14165fac05930cbeaee42a75863200b03e39
nionroy <136985474+nionroy@users.noreply.github.com>
2023-11-18 14:06:23 +0600

    Add files via upload

---
+++ Sms.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+class Sms{
+public static void main (String[]args){
+	
+
+JOptionPane.showMessageDialog(null,"wrong password","instargram",JOptionPane.ERROR_MESSAGE);
+
+}
+
+}
