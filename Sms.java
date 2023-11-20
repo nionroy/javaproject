60cd5d0052c383dddbac7aab32638af127e1d836
nionroy <136985474+nionroy@users.noreply.github.com>
2023-11-20 12:02:09 +0600

    Update Sms.java

--- Sms.java
+++ Sms.java
@@ -1,8 +1,9 @@
+//pop up message code
 import javax.swing.JOptionPane;
 class Sms{
 public static void main (String[]args){
 	
-
+//parameters-windows open,message,title,icon before message
 JOptionPane.showMessageDialog(null,"wrong password","instargram",JOptionPane.ERROR_MESSAGE);
 
 }
